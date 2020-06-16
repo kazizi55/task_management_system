a816734803897c58a24b06d2b7af5465cbf63422
kazizi55 <kaz.redhot1201@gmail.com>
2020-06-16 19:06:31 +0900

    編集ページを作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -21,9 +22,24 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def task_params
     params.require(:task).permit(:name, :explanation)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
