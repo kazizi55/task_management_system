a3591ed7162c5fd07bf09ee3cc5aaab7f292dd19
kazizi55 <kaz.redhot1201@gmail.com>
2020-06-16 18:46:02 +0900

    タスクの作成ページを作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,24 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_params)
+
+    if @task.save
+      redirect_to tasks_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :explanation)
+  end
 end
