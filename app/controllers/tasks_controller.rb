e0d96cca39f0e3f9a93eb5344cb7093b63ee16d7
kazizi55 <kaz.redhot1201@gmail.com>
2020-06-18 13:29:12 +0900

    指摘いただいた箇所を修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,8 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update]
+  before_action :find_task, only: [:show, :edit, :update]
 
   def index
     @tasks = Task.all
-  end
-
-  def show
   end
 
   def new
@@ -14,25 +11,23 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.create(task_params)
-
-    if @task.save
-      redirect_to tasks_path, notice: "タスクを追加しました"
-    else
-      flash.now[:alert] = "登録に失敗しました"
-      render 'new'
-    end
-  end
-
-  def edit
+    @task.save
+    redirect_to tasks_path, notice: "タスクを追加しました" 
+  rescue => e
+    logger.error e 
+    logger.error e.backtrace.join("\n") 
+    flash.now[:alert] = "登録に失敗しました"
+    render 'new'
   end
 
   def update
-    if @task.update(task_params)
-      redirect_to tasks_path, notice: "タスクを修正しました"
-    else
-      flash.now[:alert] = "更新に失敗しました"
-      render 'edit'
-    end
+    @task.update(task_params)
+    redirect_to tasks_path, notice: "タスクを修正しました"
+  rescue => e
+    logger.error e 
+    logger.error e.backtrace.join("\n") 
+    flash.now[:alert] = "更新に失敗しました"
+    render 'edit'
   end
 
   private
@@ -41,7 +36,7 @@ class TasksController < ApplicationController
     params.require(:task).permit(:name, :explanation)
   end
 
-  def set_task
+  def find_task
     @task = Task.find(params[:id])
   end
 end
