3a75621b3f6c8c2c8f78ec668b6b34866bdfe38f
kazizi55 <kaz.redhot1201@gmail.com>
2020-06-23 15:07:19 +0900

    終了期限にバリデーションを追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,8 @@
 class Task < ApplicationRecord
+    validates :deadline, presence: true
+    validate :deadline_cannot_be_in_the_past
+
+    def deadline_cannot_be_in_the_past
+    errors.add(:deadline, "は過去の日時は選択できません") if deadline < Time.now
+    end
 end
