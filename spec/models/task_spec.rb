7b97040d71f44b1a9363b7ce974a97a3d67ed2fa
kazizi55 <kaz.redhot1201@gmail.com>
2020-06-24 17:19:03 +0900

    rspecを修正

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,9 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  it 'タスク名が空の場合は無効' do
+  context 'タスク名が空の場合' do
     task = Task.new(name: nil, explanation: 'テストテスト')
     task.valid?
-    expect(task.errors[:name]).to include('を入力してください')
+    
+    it 'エラーメッセージを返す' do
+      expect(task.errors[:name]).to include('を入力してください')
+    end
   end
 end
