2a44e5a306865ae13467fe07fcb637787fb1fb6b
kazizi55 <kaz.redhot1201@gmail.com>
2020-06-25 12:10:58 +0900

    specファイルの表記を修正

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,25 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  describe 'バリデーション' do
+  describe 'validation test' do
     let(:task) { described_class.new(params) }
     subject { task.valid? }
 
-    describe '正常系' do
-      context '問題ない場合' do
-        let(:params) { { name: 'テスト', explanation: 'テストテスト' } }
+    context 'when success' do
+      let(:params) { { name: 'テスト', explanation: 'テストテスト' } }
 
-        it 'バリデーションを通過' do
-          is_expected.to eq true
-        end
+      it 'validates successfully' do
+        is_expected.to eq true
       end
     end
 
-    describe '異常系' do
-      context 'カラム名が空の場合' do
+    context 'when fail' do
+      context 'name column is nil' do
         let(:params) { { name: nil, explanation: 'テストテスト' } }
 
-        it 'エラーメッセージを返す' do
+        it 'returns an error message' do
           is_expected.to eq false
           expect(task.errors[:name]).to include('を入力してください')
         end
