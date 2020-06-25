abff075e7f62f3b1c1af595875aa7f8eac9308af
kazizi55 <kaz.redhot1201@gmail.com>
2020-06-25 11:57:08 +0900

    再度ご指摘いただいた箇所を修正

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,26 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  let(:task) { described_class.new(params) }
-  subject { task.valid? }
+  describe 'バリデーション' do
+    let(:task) { described_class.new(params) }
+    subject { task.valid? }
 
-  describe '正常系' do
-    context '問題ない場合' do
-      let(:params) { { name: 'テスト', explanation: 'テストテスト' } }
+    describe '正常系' do
+      context '問題ない場合' do
+        let(:params) { { name: 'テスト', explanation: 'テストテスト' } }
 
-      it 'バリデーションを通過' do
-        is_expected.to eq true
+        it 'バリデーションを通過' do
+          is_expected.to eq true
+        end
       end
     end
-  end
 
-  describe '異常系' do
-    context 'カラム名が空の場合' do
-      let(:params) { { name: nil, explanation: 'テストテスト' } }
+    describe '異常系' do
+      context 'カラム名が空の場合' do
+        let(:params) { { name: nil, explanation: 'テストテスト' } }
 
-      it 'エラーメッセージを返す' do
-        is_expected.to eq false
-        expect(task.errors[:name]).to include('を入力してください')
+        it 'エラーメッセージを返す' do
+          is_expected.to eq false
+          expect(task.errors[:name]).to include('を入力してください')
+        end
       end
     end
   end
