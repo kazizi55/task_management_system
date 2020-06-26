4233fdba884fe5bad3f88579f63af4408936ad45
kazizi55 <kaz.redhot1201@gmail.com>
2020-06-26 18:04:32 +0900

    ご指摘いただいた箇所をsystem_spec以外修正

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,10 +1,5 @@
 class Task < ApplicationRecord
   validates :name, presence: true
   validates :deadline, presence: true
-  validate :deadline_cannot_be_in_the_past
-
-  def deadline_cannot_be_in_the_past
-    return if deadline == nil
-    errors.add(:deadline, "は過去の日時は選択できません") if deadline < Time.now 
-  end
+  validates :deadline, datatime_past: true
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -9,16 +9,16 @@ RSpec.describe Task, type: :model do
       let(:params) { { name: 'テスト', explanation: 'テストテスト', deadline: Time.current.tomorrow } }
 
       it 'validates successfully' do
-        is_expected.to eq true
+        expect(subject).to eq true
       end
     end
 
     context 'when fail' do
       context 'name is nil' do
-        let(:params) { { name: nil, explanation: 'テストテスト' } }
+        let(:params) { { name: nil, explanation: 'テストテスト', deadline: Time.current.tomorrow } }
 
         it 'returns an error message' do
-          is_expected.to eq false
+          expect(subject).to eq false
           expect(task.errors[:name]).to include('を入力してください')
         end
       end
@@ -27,7 +27,7 @@ RSpec.describe Task, type: :model do
         let(:params) { { name: 'テスト', explanation: 'テストテスト', deadline: nil } }
 
         it 'returns an error message' do
-          is_expected.to eq false
+          expect(subject).to eq false
           expect(task.errors[:deadline]).to include('を入力してください')
         end
       end
@@ -36,7 +36,7 @@ RSpec.describe Task, type: :model do
         let(:params) { { name: 'テスト', explanation: 'テストテスト', deadline: Time.current.yesterday } }
 
         it 'returns an error message' do
-          is_expected.to eq false
+          expect(subject).to eq false
           expect(task.errors[:deadline]).to include('は過去の日時は選択できません')
         end
       end
