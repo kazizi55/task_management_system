3653017652ef06eb5b40e82f2b9a324af8dac5df
kazizi55 <kaz.redhot1201@gmail.com>
2020-06-26 13:24:31 +0900

    system_specを追加、修正

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,16 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe 'Tasks', type: :system, js: true do
-    it "作成日時の順番で並び替えができていること" do
-        Task.destroy_all
-        Task.create(id: 1, name: 'テスト1', explanation: 'hi')
-        Task.create(id: 2, name: 'テスト2', explanation: 'hihi2', created_at: Time.current + 1.days)
-        Task.create(id: 3, name: 'テスト3', explanation: 'hihi3', created_at: Time.current + 2.days)
-        Task.create(id: 4, name: 'テスト4', explanation: 'hihi4', created_at: Time.current + 3.days)
-        visit tasks_path
+  describe 'order test in index' do
+    Task.destroy_all # 順番の正しさをタスク名で判断するため、全タスクを削除
+    Task.create(id: 1, name: 'テスト1', explanation: 'hi', deadline: Time.current.tomorrow)
+    Task.create(id: 2, name: 'テスト2', explanation: 'hihi2', deadline: Time.current + 2.days, created_at: Time.current + 1.days)
+    Task.create(id: 3, name: 'テスト3', explanation: 'hihi3', deadline: Time.current + 3.days, created_at: Time.current + 2.days)
+    Task.create(id: 4, name: 'テスト4', explanation: 'hihi4', deadline: Time.current + 4.days, created_at: Time.current + 3.days)
+
+    before do
+      visit tasks_path
+    end
+
+    context 'by default' do
+      it 'sort by created_at' do
         task = all('h2')
         task_0 = task[0]
         expect(task_0).to have_content "テスト4"
         save_and_open_page
+      end
     end
+
+    context 'click 期限順' do
+      it 'sort by created_at' do
+        click_on '期限順'
+        task = all('h2')
+        task_0 = task[0]
+        expect(task_0).to have_content "テスト1"
+        save_and_open_page
+      end
+    end
+  end
 end
