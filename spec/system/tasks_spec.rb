59a8c4ec3bad9743040ec795b8cc0597425b1aa3
kazizi55 <kaz.redhot1201@gmail.com>
2020-06-27 23:25:53 +0900

    system_spec周りのご指摘を反映

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -2,13 +2,11 @@ require 'rails_helper'
 
 RSpec.describe 'Tasks', type: :system, js: true do
   describe 'order test in index' do
-    Task.destroy_all # 順番の正しさをタスク名で判断するため、全タスクを削除
-    Task.create(id: 1, name: 'テスト1', explanation: 'hi', deadline: Time.current.tomorrow)
-    Task.create(id: 2, name: 'テスト2', explanation: 'hihi2', deadline: Time.current + 2.days, created_at: Time.current + 1.days)
-    Task.create(id: 3, name: 'テスト3', explanation: 'hihi3', deadline: Time.current + 3.days, created_at: Time.current + 2.days)
-    Task.create(id: 4, name: 'テスト4', explanation: 'hihi4', deadline: Time.current + 4.days, created_at: Time.current + 3.days)
+    before(:all) do
+      create_list(:task, 4)
+    end
 
-    before do
+    before(:each) do
       visit tasks_path
     end
 
