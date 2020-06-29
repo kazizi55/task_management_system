4ffc64d6c3da9218a5ff3cfcd337e2ae318d2c1f
kazizi55 <kaz.redhot1201@gmail.com>
2020-06-29 14:23:50 +0900

    タスク名とステータスに検索インデックスを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+20.times do |n|
+  Task.create!(
+    name: "テスト未着手#{n + 1}",
+    explanation: "テストテスト",
+    status: 0,
+    deadline: Time.current + (n + 1).days
+  )
+
+  Task.create!(
+    name: "テスト着手中#{n + 1}",
+    explanation: "テストテスト",
+    status: 1,
+    deadline: Time.current + (n + 1).days
+  )
+
+  Task.create!(
+    name: "テスト完了#{n + 1}",
+    explanation: "テストテスト",
+    status: 2,
+    deadline: Time.current + (n + 1).days
+  )
+end
