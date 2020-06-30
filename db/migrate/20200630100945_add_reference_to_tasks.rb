02b0ca1d9306c45f64d0ba6df8a44ea32f3748e7
kazizi55 <kaz.redhot1201@gmail.com>
2020-06-30 19:13:25 +0900

    userモデルを作成してリレーション実装

---
+++ db/migrate/20200630100945_add_reference_to_tasks.rb
@@ -0,0 +1,5 @@
+class AddReferenceToTasks < ActiveRecord::Migration[5.2]
+  def change
+    add_reference  :tasks,  :user
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_23_030603) do
+ActiveRecord::Schema.define(version: 2020_06_30_100945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 2020_06_23_030603) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deadline", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
