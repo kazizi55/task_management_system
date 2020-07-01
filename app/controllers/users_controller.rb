421d1a9361fde015e24a2671a21de3aeafba2349
kazizi55 <kaz.redhot1201@gmail.com>
2020-07-01 21:43:46 +0900

    ユーザー登録機能を実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.valid?
+    @user.save!
+    redirect_to login_path, notice: "ユーザーを登録しました" 
+  rescue => e
+    logger.error e 
+    logger.error e.backtrace.join("\n") 
+    flash.now[:alert] = "登録に失敗しました"
+    render 'new'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :mail, :password, :password_confirmation)
+  end
+end
