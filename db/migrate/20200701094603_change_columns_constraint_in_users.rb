21acc8f2e35c2e0961285ccf6924549dfb1f2b2c
kazizi55 <kaz.redhot1201@gmail.com>
2020-07-01 18:49:14 +0900

    ログイン・ログアウト用のカラムとバリデーションを追加

---
+++ db/migrate/20200701094603_change_columns_constraint_in_users.rb
@@ -0,0 +1,6 @@
+class ChangeColumnsConstraintInUsers < ActiveRecord::Migration[5.2]
+  def change
+    change_column_null :users, :email, false
+    change_column_null :users, :password_digest, false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_30_100945) do
+ActiveRecord::Schema.define(version: 2020_07_01_094603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,9 @@ ActiveRecord::Schema.define(version: 2020_06_30_100945) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.string "remember_token"
   end
 
 end
