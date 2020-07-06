9b5ab66e8ed641b94857d1cd2cb031abe2d2aaee
kazizi55 <kaz.redhot1201@gmail.com>
2020-07-06 15:47:05 +0900

    メソッドを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
-  rescue_from Exception,                        with: :_render_500
-  rescue_from ActiveRecord::RecordNotFound,     with: :_render_404
-  rescue_from ActionController::RoutingError,   with: :_render_404
+  unless Rails.env.development?
+    rescue_from Exception,                      with: :render_500
+    rescue_from ActiveRecord::RecordNotFound,   with: :render_404
+    rescue_from ActionController::RoutingError, with: :render_404
+  end
 
   def routing_error
     raise ActionController::RoutingError, params[:path]
@@ -9,23 +11,11 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def _render_404(e = nil)
-    logger.info "Rendering 404 with exception: #{e.message}" if e
-
-    if request.format.to_sym == :json
-      render json: { error: '404 error' }, status: :not_found
-    else
-      render 'errors/404', status: :not_found
-    end
+  def render_404
+    render 'errors/404', status: :not_found
   end
 
-  def _render_500(e = nil)
-    logger.error "Rendering 500 with exception: #{e.message}" if e
-
-    if request.format.to_sym == :json
-      render json: { error: '500 error' }, status: :internal_server_error
-    else
-      render 'errors/500', status: :internal_server_error
-    end
+  def render_500
+    render 'errors/500', status: :internal_server_error
   end
 end
