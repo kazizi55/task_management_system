d7d784813e10764fc1d3f2964b9a65d637e4ceb8
kazizi55 <kaz.redhot1201@gmail.com>
2020-07-06 18:28:23 +0900

    テストデータを更新

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,36 @@
-User.create!(
-   name: 'テスト太郎'
-)
-
-20.times do |n|
-  Task.create!(
-    name: "テスト未着手#{n + 1}",
-    explanation: "テストテスト",
-    status: "not_started",
-    deadline: Time.current + (n + 1).days
+3.times do |n|
+  User.create!(
+    id: n + 1,
+    name: "テスト太郎#{n + 1}",
+    email: "test#{n + 1}@test.com",
+    password: "123456".crypt(Rails.application.credentials.salt[:salt_key])
   )
+end
 
-  Task.create!(
-    name: "テスト着手中#{n + 1}",
-    explanation: "テストテスト",
-    status: "in_progress",
-    deadline: Time.current + (n + 1).days
-  )
+5.times do |m|
+  3.times do |n|
+    Task.create!(
+      name: "テスト未着手#{m + 1}",
+      explanation: "テストテスト",
+      status: "not_started",
+      deadline: Time.current + (m + 1).days,
+      user_id: n + 1
+    )
 
-  Task.create!(
-    name: "テスト完了#{n + 1}",
-    explanation: "テストテスト",
-    status: "completed",
-    deadline: Time.current + (n + 1).days
-  )
+    Task.create!(
+      name: "テスト着手中#{m + 1}",
+      explanation: "テストテスト",
+      status: "in_progress",
+      deadline: Time.current + (m + 1).days,
+      user_id: n + 1
+    )
+
+    Task.create!(
+      name: "テスト完了#{m + 1}",
+      explanation: "テストテスト",
+      status: "completed",
+      deadline: Time.current + (m + 1).days,
+      user_id: n + 1
+    )
+  end
 end
