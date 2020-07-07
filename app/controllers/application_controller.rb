134db016bc386f24e00048bb466f6502095a11cf
kazizi55 <kaz.redhot1201@gmail.com>
2020-07-07 17:47:04 +0900

    エラーページを静的にするのに方針変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,2 @@
 class ApplicationController < ActionController::Base
-  unless Rails.env.development?
-    rescue_from Exception,                      with: :render_500
-    rescue_from ActiveRecord::RecordNotFound,   with: :render_404
-    rescue_from ActionController::RoutingError, with: :render_404
-  end
-
-  def routing_error
-    raise ActionController::RoutingError, params[:path]
-  end
-
-  private
-
-  def render_404
-    render 'errors/404', status: :not_found
-  end
-
-  def render_500
-    render 'errors/500', status: :internal_server_error
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
   resources :tasks
-
-  get '*not_found' => 'application#routing_error'
-  post '*not_found' => 'application#routing_error'
 end
